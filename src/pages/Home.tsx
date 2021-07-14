dfe7aef5339ef6f5912632922cfee8b2aad79e22
jacksonadh <66038167+jacksonadh@users.noreply.github.com>
2021-07-14 11:09:36 -0300

    Primeiro commit
    Projeto do desafio #1 do hiring coders

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import '../styles/home.scss'
+import logo from '../assets/images/logoHorizontalBranca.png'
+export default function Home() {
+  return (
+    <>
+      <img src={logo} alt="Logo da loja Ei Isy Art" />
+      <h1>BLACK <span>FRIDAY</span></h1>
+      <p>A MAIOR DO BRASIL É AQUI!</p>
+    </>
+  )
+}
