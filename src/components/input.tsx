6d274079fc2e77209a4b81ad7aa122c02455aca6
jacksonadh <66038167+jacksonadh@users.noreply.github.com>
2021-07-17 12:01:35 -0300

    Configurando input para salvar dados de e-mail no localstorage

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import '../styles/input.scss';
 
 
 export default function Input() {
-
+  const [email, setEmail] = useState("")
+  const armazenar = (chave: string, email: string) => {
+    localStorage.setItem(chave, email)
+  }
   return (
 
     <>
-      <input type="email" placeholder="Digite seu melhor e-mail" />
-      <button type="submit">Cadastrar</button>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Digite seu melhor e-mail" />
+      <button type="submit" onClick={() => armazenar("Email: ", email)} >Cadastrar</button>
     </>
   )
 }
+
 
 
