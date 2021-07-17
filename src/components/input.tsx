9bae0abe2d2e22e7e64b18cc102c66ea43f44e12
jacksonadh <66038167+jacksonadh@users.noreply.github.com>
2021-07-17 11:35:03 -0300

    verificando netlify

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import '../styles/input.scss';
+
 
 export default function Input() {
 
   return (
 
     <>
-      <input id="email" type="email" placeholder="Digite seu melhor e-mail" />
-      <button type="submit" >Cadastrar</button>
+      <input type="email" placeholder="Digite seu melhor e-mail" />
+      <button type="submit">Cadastrar</button>
     </>
   )
 }
+
+
